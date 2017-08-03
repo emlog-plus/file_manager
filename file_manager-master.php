ce95d93b6089b1b560f377ed7194b8d640ebde77
Flyerme <gao.eison@gmail.com>
2017-08-02 21:23:33 -0400

    Add files via upload

---
+++ file_manager-master.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: 文件管理器
+Description: 对于博客emlog简单的文件管理器
+Author: DOC_tr
+Author Email: CrimSoN_AL@mail.ru
+Author URL: http://phpbl.ru
+ForEmlog: 5.3.0+ PHP 5.3+
+*/
+!defined('EMLOG_ROOT') && exit('access deined!');
+function file_m() {
+    echo '<li><a href="./plugin.php?plugin=file_manager-master" id="menu_file">文件管理</a></li>';
+}
+addAction('adm_sidebar_ext', 'file_m');
+?>
